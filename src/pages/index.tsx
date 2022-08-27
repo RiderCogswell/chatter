b5fb5b6f1305be507cca2cb869551659c8d96fd8
Ridercogswell <ridercogswell@gmail.com>
2022-08-27 01:59:15 -0700

    mvp front end

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import { useState, useEffect } from 'react';
 
-let socket;
+let socket: { on: (arg0: string, arg1: (msg: { author: any; message: any; }) => void) => void; emit: (arg0: string, arg1: { author: string; message: string; }) => void; };
 
 type Message = {
   author: string;
@@ -24,7 +24,7 @@ export default function Home() {
 
     socket = io();
 
-    socket.on('newIncomingMessage', (msg: { author: any; message: any; }) => {
+    socket.on('newIncomingMessage', (msg: Message) => {
       setMessages((currentMsg) => [
         ...currentMsg, // doesnt affect other msgs
         { author: msg.author, message: msg.message },
@@ -40,5 +40,83 @@ export default function Home() {
       { author: chosenUsername, message },
     ]);
     setMessage('')
-  }
+  };
+   
+  const handleKeyPress = (e: { keyCode: number; }) => {
+    // it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      if (message) {
+        sendMessage();
+      }
+    }
+  };
+
+  return (
+    <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
+      <main className="gap-4 flex flex-col items-center justify-center w-full h-full">
+        {!chosenUsername ? (
+          <>
+            <h3 className="font-bold text-white text-xl">
+              How people should call you?
+            </h3>
+            <input
+              type="text"
+              placeholder="Identity..."
+              value={username}
+              className="p-3 rounded-md outline-none"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                setChosenUsername(username);
+              }}
+              className="bg-white rounded-md px-4 py-2 text-xl"
+            >
+              Go!
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="font-bold text-white text-xl">
+              Your username: {username}
+            </p>
+            <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
+              <div className="h-full last:border-b-0 overflow-y-scroll">
+                {messages.map((msg, i) => {
+                  return (
+                    <div
+                      className="w-full py-1 px-2 border-b border-gray-200"
+                      key={i}
+                    >
+                      {msg.author} : {msg.message}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="border-t border-gray-300 w-full flex rounded-bl-md">
+                <input
+                  type="text"
+                  placeholder="New message..."
+                  value={message}
+                  className="outline-none py-2 px-2 rounded-bl-md flex-1"
+                  onChange={(e) => setMessage(e.target.value)}
+                  onKeyUp={handleKeyPress}
+                />
+                <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
+                  <button
+                    className="group-hover:text-white px-3 h-full"
+                    onClick={() => {
+                      sendMessage();
+                    }}
+                  >
+                    Send
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </main>
+    </div>
+  );
 }
